9a25f311d7fa7067327e4b3daf490e65b6041ee4
YagoGefaellOficial <yaguetegpl@gmail.com>
2025-04-23 18:31:30 +0200

    Cotinue of the as commit.

--- src/main/java/com/mtg/magicapi/Card/CardController.java
+++ src/main/java/com/mtg/magicapi/Card/CardController.java
@@ -11,11 +11,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
-
 @Controller
 @RequestMapping("/home")
-public class CardViewController {
+public class CardController {
 
     @Autowired
     private CardService cardService;
